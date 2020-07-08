baa8692e2163ad3f21d9f38b9d22c08ade2d941e
thegabbythemaster <gabby@DESKTOP-TOMMG36.localdomain>
2020-07-07 22:42:08 -0400

    Added name Gabby

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Name: Gabby Gonzalez</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'salmon',
     alignItems: 'center',
     justifyContent: 'center',
   },
