27e6f879472a1e98c5232627b4e7e66b41b467e2
William Bogans <william.bogans@williambogans.dev>
2020-07-10 23:20:40 -0400

    [add] - can create list for home screen contacts

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,60 +5,82 @@ import { ContactContext } from '../context/ContactContext';
 
 const HomeScreen = ({ navigation }) => {
   const { contacts } = useContext(ContactContext);
-  const renderItem = ({ item }) => (
-    <View style={{ backgroundColor: 'red' }}>
-      <Text>{item.name}</Text>
-    </View>
-  );
 
-  function renderContent() {
-    if (!contacts.length) {
-      return (
-        <SafeAreaView
+  const renderItem = ({ item: { contactInfo } }) => {
+    console.log(contactInfo);
+    return (
+      <View
+        style={{
+          borderColor: 'black',
+          borderWidth: 1,
+          marginVertical: 5,
+          marginHorizontal: 20,
+          display: 'flex',
+        }}
+      >
+        <View
           style={{
-            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
             alignItems: 'center',
-            justifyContent: 'center',
           }}
         >
-          <Text>You haven't added any contacts yet!!</Text>
-          <TouchableOpacity
+          <Text style={{ fontSize: 26 }}>{contactInfo.name}</Text>
+          <Text>{contactInfo.phoneNumber}</Text>
+        </View>
+        <Text style={{ alignSelf: 'center', fontSize: 24 }}>
+          [Contact Date Here]
+        </Text>
+      </View>
+    );
+  };
+
+  if (!contacts.length) {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text>You haven't added any contacts yet!!</Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#01a9b4',
+            width: 150,
+            height: 40,
+            borderRadius: 3,
+            display: 'flex',
+            alignSelf: 'center',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}
+          onPress={() => navigation.navigate('Add Contact')}
+        >
+          <Text
             style={{
-              backgroundColor: '#01a9b4',
-              width: 150,
-              height: 40,
-              borderRadius: 3,
-              display: 'flex',
-              alignSelf: 'center',
-              justifyContent: 'center',
-              marginTop: 20,
+              textAlign: 'center',
+              color: 'white',
             }}
-            onPress={() => navigation.navigate('Add Contact')}
           >
-            <Text
-              style={{
-                textAlign: 'center',
-                color: 'white',
-              }}
-            >
-              Create new Contact
-            </Text>
-          </TouchableOpacity>
-        </SafeAreaView>
-      );
-    } else {
-      return (
-        <SafeAreaView>
-          <FlatList
-            data={contacts}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-          />
-        </SafeAreaView>
-      );
-    }
+            Create new Contact
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList
+          data={contacts}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={contacts}
+        />
+      </SafeAreaView>
+    );
   }
-  return renderContent();
 };
 
 export default HomeScreen;
--- src/context/ContactContext.js
+++ src/context/ContactContext.js
@@ -1,22 +1,24 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useRef } from 'react';
 
 export const ContactContext = createContext();
 
 const ContactProvider = ({ children }) => {
-  const [contacts, setContacts] = useState([
-    {
-      id: '0',
-      name: 'William',
-      phoneNumber: '9124019367',
-    },
-  ]);
+  const [contacts, setContacts] = useState([]);
+  const id = useRef(0);
 
   function addContact(contactInfo) {
-    setContacts({ ...contacts, contactInfo });
+    console.log(contactInfo);
+    setContacts([...contacts, { contactInfo, id: String(id.current) }]);
+    id.current++;
+    console.log(contacts);
+  }
+
+  function deleteContact(id) {
+    setContacts(contacts.filter((contact) => contact.id === id));
   }
 
   return (
-    <ContactContext.Provider value={{ addContact, contacts }}>
+    <ContactContext.Provider value={{ addContact, contacts, deleteContact }}>
       {children}
     </ContactContext.Provider>
   );
