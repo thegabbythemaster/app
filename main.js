40c4799e189d9f198bc5d380329cc4e2d2494f5c
Janina <janinatalvarez@Gmail.com>
2020-07-22 12:25:31 -0700

    Made an if statement to show certain divs depending on whether contact list exists. New issue is-- can't get the contacts list array to stay in local storage, it currently resets every time the page is refreshed or changed

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-let contacts = [];
 
-let empty = document.getElementById('');
-function submit() {
-    let ContactName = document.getElementById('input');
-    let ContactPhoneNumber = document.getElementById('input');
-    
-    let person = {
-        name: ContactName,
-        phone: ContactPhoneNumber,
-    };
-    contacts.push(person);
-};
-
-if(contacts.length === 0 || contacts === undefined){
- console.log(empty);
+/*if the contacts array doesn't exist in local storage, then create a contacts array and put it in local storage*/
+if (localStorage.getItem("mycontacts") === null){
+    window.alert('it is null'); //for testing
+    var contacts = [];
+    localStorage.setItem("mycontacts",JSON.stringify(contacts)); //only strings can be stored in local storage, so this turns the array into a string
 }
 else{
-    console.log(contacts);
+    window.alert('not null'); //for testing
 }
 
+function add_contact() {
+    var person = {
+    contactname: document.getElementById('CName'),
+    contactnumber: document.getElementById('CPhone')
+    }
+
+    let people = JSON.parse(localStorage.getItem("mycontacts"));
+
+    people.push(person);
+
+    window.alert(people.size); //for testing-- I'm getting undefined, so there must be some issue turning the string back into an array
+}
 
