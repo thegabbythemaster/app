dbb1e466dd05c3f449727b6cf177da77a71f7e7b
Janina <janinatalvarez@Gmail.com>
2020-07-30 15:27:04 -0700

    added gabby's tab navigator styling

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react';
 import HomeScreen from './src/components/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateContactScreen from './src/components/CreateContactScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import ContactProvider from './src/context/ContactContext';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, Image } from 'react-native';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +18,17 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <View>
+        <View style={{paddingTop: 50, alignItems: 'center', justifyContent: 'center'}}>
+        </View>
+        <Button
+          color = '#716992'
+          title="Press to Send Notification"
+          onPress={async () => {
+            await sendPushNotification(expoPushToken);
+          }}
+        />
+      </View>
       <ContactProvider>
         <Tab.Navigator
           screenOptions={({ route }) => ({
@@ -23,9 +39,15 @@ export default function App() {
               } else if (route.name === 'Add Contact') {
                 iconName = 'ios-add-circle-outline';
               }
-              return <Ionicons name={iconName} size={size} color={color} />;
+              return <Ionicons name={iconName} size={size} color={'#716992'} />;
             },
           })}
+          tabBarOptions={{
+            color: 'black',
+            activeTintColor: '#fcf7e1',
+            inactiveTintColor: 'gray',
+            activeBackgroundColor: '#abc7b9'
+          }}
         >
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Add Contact" component={CreateContactScreen} />
